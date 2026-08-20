889d47281b8dd61d5413cc362d2607ab52c4fce0
UJJWAL PRAKASH <ujjwallp22@gmail.com>
2026-08-21 01:01:56 +0530

    Delete folder C_Book

--- C_Book/C1_p1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a, b;
-    printf("Enter the two numbers a and b: ");
-    scanf("%d %d", &a , &b);
-    int c = a + b;
-    printf("The sum of a and b is: %d ", c);
-    return 0;
-}
--- C_Book/Pointers/ques01b.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) {
-    double u,v;
-    u = 0.3E45;
-    double * pu;
-    double * pv;
-    
-}
