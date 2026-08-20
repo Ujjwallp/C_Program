45f1ac77f8d00a4268e32c42627f463f5b840246
UJJWAL PRAKASH <ujjwallp22@gmail.com>
2026-08-21 00:58:15 +0530

    Update C1_p1.c

--- C_Book/C1_p1.c
+++ C_Book/C1_p1.c
@@ -2,7 +2,7 @@
 int main() {
     int a, b;
     printf("Enter the two numbers a and b: ");
-    scanf("%d % d", &a , &b);
+    scanf("%d %d", &a , &b);
     int c = a + b;
     printf("The sum of a and b is: %d ", c);
     return 0;
